8cd2abe1d637fd35669cbbbf9827d38b725be1c9
Steve Kelly <kd2cca@gmail.com>
2014-09-26 11:04:37 -0400

    initial scripts

---
+++ julia-multi.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo -e "\e[92mExporting pkg path as:\e[0m"
+export JULIA_PKGDIR="/home/steve/.julia/v0.4/"
+echo $JULIA_PKGDIR
+
+echo -e "\e[92mRunning on Julia release-0.3\e[0m"
+julia3 $@
+
+echo -e "\e[92mRunning on Julia master\e[0m"
+julia $@
+
+
+
